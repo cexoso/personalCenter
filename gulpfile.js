eb36c34946d5d5ec1a2bdb6ec8ddc9e2182c8b50
cexoso <285516193@qq.com>
2015-09-21 06:13:46 +0800

    nd

--- gulpfile.js
+++ gulpfile.js
@@ -1,170 +1,223 @@
 'use strict'
 var gulp = require('gulp'),
-connect = require('gulp-connect'),
-jshint = require('gulp-jshint'),
-concat = require('gulp-concat'),
-jsmin = require('gulp-jsmin'),
-es = require('event-stream'),
-inject = require('gulp-inject'),
-bowerFiles = require('main-bower-files'),
-csso = require('gulp-csso'),
-rename = require('gulp-rename'),
-sass = require("gulp-sass"),
-path = require("path"),
-rimraf = require("gulp-rimraf"),
-chokidar = require('chokidar'),
-autoprefixer=require('gulp-autoprefixer');
-gulp.task('server', ['injectDev','scss:watch'],
-function() {
+    connect = require('gulp-connect'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    jsmin = require('gulp-jsmin'),
+    es = require('event-stream'),
+    inject = require('gulp-inject'),
+    bowerFiles = require('main-bower-files'),
+    csso = require('gulp-csso'),
+    rename = require('gulp-rename'),
+    sass = require("gulp-sass"),
+    path = require("path"),
+    rimraf = require("gulp-rimraf"),
+    chokidar = require('chokidar'),
+    rev = require('gulp-rev'),
+    uglify = require('gulp-uglify'),
+    es = require('event-stream'),
+    gulpFilter = require('gulp-filter'),
+    sort = require('gulp-sort'),
+    Q = require('q'),
+    templateCache = require('gulp-angular-templatecache'),
+    autoprefixer = require('gulp-autoprefixer');
+gulp.task('server', ['scss:watch'], function () {
     connect.server({
         root: ['app', 'bower_components'],
         livereload: true,
         port: 80,
         middleware: function (connect, opt) {
-          return [function (req, res, next) {
-            if (!req.url.match('/api/')) {
-              next();
-            } else {
-              console.log('代理：' + req.url)
-              var method=req.method;
-              req.method='GET';              
-              req.url+=('-'+method+'.json')
-              console.log(req.url);
-              next();              
-            }
-          }
-          ]
-        }//end of middleware
+                return [
+
+                    function (req, res, next) {
+                        if(!req.url.match('/api/')) {
+                            next();
+                        }
+                        else {
+                            console.log('代理：' + req.url)
+                            var method = req.method;
+                            req.method = 'GET';
+                            req.url += ('-' + method + '.json')
+                            console.log(req.url);
+                            next();
+                        }
+                    }
+                ]
+            } //end of middleware
     });
     var html = 'app/**/*.html';
-    var css='app/**/*.css';
-    var js='app/**/*.js';
-    chokidar.watch([].concat(css,js))
-    .on('add',injectDev)
-    .on('unlink',injectDev);
-    chokidar.watch([].concat(css,js,html))
-    .on('all',function(event,path){
-      console.log(path+"-change");
-      gulp.src('./app/index.html').pipe(connect.reload());
-    })
+    var css = 'app/**/*.css';
+    var js = 'app/**/*.js';
+    chokidar.watch([].concat(css, js))
+        .on('add', injectDev)
+        .on('unlink', injectDev);
+    chokidar.watch([].concat(css, js, html))
+        .on('all', function (event, path) {
+            gulp.src('./app/index.html')
+                .pipe(connect.reload());
+        })
 });
-gulp.task('lint',
-function() {
-    var js = ['app/scripts/**/*.js'];
-    gulp.watch(js,
-    function() {
-        gulp.src(js).pipe(jshint()).pipe(jshint.reporter('default'));
-    });
-});
+function clear() {
+    return gulp.src('dist')
+        .pipe(rimraf({
+            force: true
+        }));
+};
+function distLogic() {
+    return gulp.src('app/**/*.js')
+        .pipe(sort())
+        .pipe(concat('logic.js'), {
+            newLine: ';'
+        })
+        // .pipe(jsmin())
+        // .pipe(uglify())
+        // .pipe(rev())
+        .pipe(gulp.dest('./dist/scripts'));
+};
+function dist_css() {
+    var filter = gulpFilter('*.css');
+    var libcss = gulp.src(bowerFiles())
+        .pipe(filter);
+    var mycss = gulp.src('app/**/*.scss')
+        .pipe(sass()
+            .on('error', sass.logError));
+    return es.merge(libcss, mycss)
+        .pipe(sort())
+        .pipe(concat('css.css'), {
+            newLine: ';'
+        })
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(csso())
+        .pipe(rev())
+        .pipe(gulp.dest('dist/css'));
+};
+function makeCache() {
+    return gulp.src('app/components/**/*.html')
+        .pipe(sort())
+        .pipe(templateCache())
+        .pipe(jsmin())
+        .pipe(rev())
+        .pipe(gulp.dest('./dist/scripts'));
+};
+function dist_html() {
+    return gulp.src('app/indexTpl.html')
+        .pipe(inject(gulp.src(['dist/**/*.css', 'dist/scripts/**/*.js'], {
+            read: false
+        }).pipe(sort()), {
+            name: 'inject',
+            addRootSlash: false,
+            ignorePath: ['dist']
+        }))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('dist/'));
+};
+function dist(){
+    gulp.start('dist_html');
+};
+function distlib() {
+    var filter = gulpFilter('*.js');
+    return gulp.src(bowerFiles())
+        .pipe(filter)
+        // .pipe(sort())
+        .pipe(concat('lib.js'), {
+            newLine: ';'
+        })
+        // .pipe(jsmin())
+        // .pipe(uglify())
+        // .pipe(rev())
+        .pipe(gulp.dest('./dist/scripts'));
+};
+gulp.task('dist_html',['makeCache','distlib','distLogic','dist_css'],dist_html);
+gulp.task('makeCache', makeCache);
+gulp.task('distLogic',distLogic);
+gulp.task('distlib',distlib);
+gulp.task('dist_css',dist_css);
+gulp.task('clear', clear);
+gulp.task('dist', ['clear'], dist);
 gulp.task('default', ['server']);
-var distPath = 'D:/apache-tomcat-7.0.63/webapps/PaticaService';
-var distPath1 = 'E:/Program Files/eclipseWS/PaticaService/WebContent';
-gulp.task('watch',
-function() {
-    gulp.watch('app/scripts/**/*.js', ['buildlogic']);
-    gulp.watch('app/views/**/*', ['buildview']);
-    gulp.watch('app/tpls/**/*', ['buildtpl']);
-    gulp.watch('app/styles/**/*.css', ['buildcsscos']);
-});
-gulp.task('build',
-function() {
-    return gulp.start(['buildlogic', 'buildview', 'buildtpl', 'buildlib', 'buildcsslib', 'buildcsscos', 'buildimg']);
-});
-gulp.task('buildlogic',
-function() {
-    var path = 'app/scripts/**/*.js';
-    return gulp.src(path).pipe(concat('all.js'), {
-        newLine: ';'
-    })
-    // .pipe(jsmin())
-    .pipe(gulp.dest(distPath + '/scripts')).pipe(gulp.dest(distPath1 + '/scripts'));
-});
-gulp.task('buildview',
-function() {
-    return gulp.src('app/views/**/*').pipe(gulp.dest(distPath + '/views')).pipe(gulp.dest(distPath1 + '/views'));
-});
-gulp.task('buildimg',
-function() {
-    return gulp.src('app/images/**/*').pipe(gulp.dest(distPath + '/images')).pipe(gulp.dest(distPath1 + '/images'));
-});
-gulp.task('buildtpl',
-function() {
-    return gulp.src('app/tpls/**/*').pipe(gulp.dest(distPath + '/tpls')).pipe(gulp.dest(distPath1 + '/tpls'));
-});
-gulp.task('buildlib',
-function() {
-    var path = 'app/scripts/**/*.js';
-    var arr = ['bower_components/angular/angular.js', 'bower_components/angular-ui-router/release/angular-ui-router.js', 'bower_components/angular-resource/angular-resource.js', 'bower_components/angular-bootstrap/ui-bootstrap.js', 'bower_components/angular-bootstrap/ui-bootstrap-tpls.js', 'bower_components/ui-select/dist/select.js', 'bower_components/angular-sanitize/angular-sanitize.js'];
-    return gulp.src(arr).pipe(concat('lib.js'), {
-        newLine: ';'
-    }).pipe(jsmin()).pipe(gulp.dest(distPath + '/scripts')).pipe(gulp.dest(distPath1 + '/scripts'));
-});
-gulp.task('buildcsslib',
-function(opt, q) {
-    var arr = ['bower_components/bootstrap/dist/css/bootstrap.min.css', 'bower_components/angular-bootstrap/ui-bootstrap-csp.css', 'bower_components/ui-select/dist/select.css'];
-    gulp.src('bower_components/bootstrap/dist/fonts/*.*').pipe(gulp.dest(distPath + '/fonts')).pipe(gulp.dest(distPath1 + '/fonts'));
-    return gulp.src(arr).pipe(concat('lib.css')).pipe(csso()).pipe(gulp.dest(distPath + '/styles')).pipe(gulp.dest(distPath1 + '/styles'));
-});
-gulp.task('buildcsscos',
-function(opt, q) {
-    var path = 'app/styles/**/*.css';
-    return gulp.src(path).pipe(concat('costom.css')).pipe(csso()).pipe(gulp.dest(distPath + '/styles')).pipe(gulp.dest(distPath1 + '/styles'));
-});
-function injectDev() {
-    gulp.src('app/indexTpl.html').pipe(inject(gulp.src(bowerFiles(), {
-        read: false
-    }), {
-        name: 'bower',
-        addRootSlash: false,
-        ignorePath: ['bower_components', 'app']
-    })).pipe(inject(gulp.src('app/**/*.js', {
-        read: false
-    }), {
-        name: 'inject',
-        addRootSlash: false,
-        ignorePath: ['bower_components', 'app']
-    })).pipe(inject(gulp.src('app/**/*.css', {
-        read: false
-    }), {
-        name: 'inject',
-        addRootSlash: false,
-        ignorePath: ['bower_components', 'app']
-    })).pipe(rename('index.html')).pipe(gulp.dest('app/'));
-};
+/**
+ * 开发模式自动注入
+ */
+var injectDev = (function () {
+    var t = null;
+    var timeout = 50;
+    return function () {
+        if(t) {
+            clearTimeout(t);
+        }
+        t = setTimeout(function () {
+            gulp.src('app/indexTpl.html')
+                .pipe(inject(gulp.src(bowerFiles(), {
+                    read: false
+                }), {
+                    name: 'bower',
+                    addRootSlash: false,
+                    ignorePath: ['bower_components', 'app']
+                }))
+                .pipe(inject(gulp.src('app/**/*.js', {
+                    read: false
+                }), {
+                    name: 'inject',
+                    addRootSlash: false,
+                    ignorePath: ['bower_components', 'app']
+                }))
+                .pipe(inject(gulp.src('app/**/*.css', {
+                    read: false
+                }), {
+                    name: 'inject',
+                    addRootSlash: false,
+                    ignorePath: ['bower_components', 'app']
+                }))
+                .pipe(rename('index.html'))
+                .pipe(gulp.dest('app/'));
+        }, timeout);
+    }
+})();
 gulp.task('injectDev', injectDev);
-gulp.task('injectDist',
-function() {
-    gulp.src('app/indexTpl.html').pipe(inject(gulp.src(bowerFiles(), {
-        read: false
-    }), {
-        name: 'bower'
-    })).pipe(rename('index.html')).pipe(gulp.dest('dist/'));
-});
+/**
+        自动编译scss文件
+     */
+var scssCompile = (function () {
+    var arr = [];
+    var t = null;
+    var timeout = 50;
+    return function (csspath) {
+        if(t) {
+            clearTimeout(t);
+        }
+        if(arr.indexOf(csspath) == -1) {
+            arr.push(csspath);
+        }
+        t = setTimeout(function () {
+            var st = new Date();
+            gulp.src(arr, {
+                    base: '.'
+                })
+                .pipe(sass()
+                    .on('error', sass.logError))
+                .pipe(autoprefixer({
+                    browsers: ['last 2 versions']
+                }))
+                .pipe(gulp.dest('./'));
+            var et = new Date();
+            console.log(et - st + 'ms for compile ' + arr.length + ' sass');
+            arr.length = 0;
+        }, timeout);
+    }
+})();
 
-function scssCompile(csspath) {
-    var st = new Date();
-    gulp.src(csspath).pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers:['last 2 versions']
-    }))
-    .pipe(gulp.dest(path.dirname(csspath)));
-    var et = new Date();
-    console.log(et - st + 'ms for compile sass');
-};
-function scssRemove(csspath){
-  var csspath=path.dirname(csspath)+'\\'+path.basename(csspath,'scss')+'css';
-  gulp.src(csspath)
-    .pipe(rimraf({force: true }));
+function scssRemove(csspath) {
+    var csspath = path.dirname(csspath) + '\\' + path.basename(csspath, 'scss') + 'css';
+    gulp.src(csspath)
+        .pipe(rimraf({
+            force: true
+        }));
 }
-gulp.task('scss:watch',
-function() {
+gulp.task('scss:watch', function () {
     chokidar.watch('app/**/*.scss')
-    .on('add', scssCompile)
-    .on('change', scssCompile)
-    .on('unlink',scssRemove)
+        .on('add', scssCompile)
+        .on('change', scssCompile)
+        .on('unlink', scssRemove);
 });
-
-gulp.task('test',
-function() {
-
-})
